de97e865ddf889d2853b2890b2ff5bcfd7e344ae
runcioa <runcioantonio@gmail.com>
2016-11-20 13:04:15 +0100

    Prove Linguaggio C
    Prove Linguaggio C

---
+++ ISBN.c
@@ -0,0 +1,16 @@
+/* Visulaizzazione numeri ISBN */
+
+#include<stdio.h>
+
+int main(void)
+{
+    int gs1Prefix, groupIdentifier, publisherCode, itemNumber, checkDigit;
+
+
+
+    printf("Inserire codice ISBN con trattini XXX-X-XXX-XXXXX-X: ");
+    scanf("%d-%d-%d-%d-%d", &gs1Prefix, &groupIdentifier, &publisherCode, &itemNumber, &checkDigit);
+
+
+
+}
